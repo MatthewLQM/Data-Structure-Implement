59be273470b719a65ecc4d90471dfc91343d93fb
MatthewLQM <MatthewLQM@Gmail.com>
2017-09-29 20:19:13 +0800

    add something

--- src/main/java/com/matthewlqm/list/SinglyLinkedList.java
+++ src/main/java/com/matthewlqm/list/SinglyLinkedList.java
@@ -1,12 +1,13 @@
 package com.matthewlqm.list;
 
+import com.matthewlqm.list.listNode.ListNode;
 import com.matthewlqm.list.listNode.SinglyLinkedListNode;
 
 public class SinglyLinkedList<T> extends AbstractList<T> {
 
     private int size;
 
-    SinglyLinkedListNode head;
+    SinglyLinkedListNode<T> head;
 
     public SinglyLinkedList() {
         size = 0;
@@ -18,7 +19,84 @@ public class SinglyLinkedList<T> extends AbstractList<T> {
         head = new SinglyLinkedListNode(key);
     }
 
-    public int size() {
+    @Override
+    public int length() {
         return size;
+    }
+
+    @Override
+    public T get(int index) {
+        if(index >= size) {
+            throw new ArrayIndexOutOfBoundsException();
+        }
+        ListNode<T> iterator = head;
+        while(index > 0) {
+            iterator = iterator.getNext();
+        }
+        return iterator.getKey();
+    }
+
+    @Override
+    public T head() {
+        if(head == null) {
+            throw new ArrayIndexOutOfBoundsException();
+        }
+        return head.getKey();
+    }
+
+    @Override
+    public T tail() {
+        if(head == null) {
+            throw new ArrayIndexOutOfBoundsException();
+        }
+        ListNode<T> iterator = head;
+        while(iterator.getNext() != null) {
+            iterator = iterator.getNext();
+        }
+        return iterator.getKey();
+    }
+
+    @Override
+    public void add(T key, int index) {
+        if(index >= size - 1) {
+            throw new ArrayIndexOutOfBoundsException();
+        }
+        ListNode<T> iterator = head;
+        while(index > 0) {
+            iterator = iterator.getNext();
+            index--;
+        }
+        SinglyLinkedListNode<T> temp = new SinglyLinkedListNode<>(key, iterator.getNext());
+        iterator.setNext(temp);
+    }
+
+    @Override
+    public void push(T key) {
+        if(head == null) {
+            head = new SinglyLinkedListNode<T>(key);
+        } else {
+            ListNode<T> iterator = head;
+            while(iterator.getNext() != null) {
+                iterator = iterator.getNext();
+            }
+            SinglyLinkedListNode temp = new SinglyLinkedListNode(key);
+            iterator.setNext(temp);
+        }
+        size++;
+    }
+
+    @Override
+    public T pop() {
+        if(head == null) {
+            throw new ArrayIndexOutOfBoundsException();
+        }
+        ListNode<T> iterator = head;
+        while(iterator.getNext() != null) {
+            iterator = iterator.getNext();
+        }
+        T result = iterator.getKey();
+        iterator = null;
+        size--;
+        return result;
     }
 }
